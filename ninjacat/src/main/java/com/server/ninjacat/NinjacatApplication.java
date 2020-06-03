52f498b29ac74a9acd00a160e15aabe73b692f1b
iamnzrv <nzrvofficial@gmail.com>
2020-06-03 20:48:22 +0300

    small fixes

--- ninjacat/src/main/java/com/server/ninjacat/NinjacatApplication.java
+++ ninjacat/src/main/java/com/server/ninjacat/NinjacatApplication.java
@@ -21,22 +21,4 @@ public class NinjacatApplication {
   public static void main(String[] args) {
     SpringApplication.run(NinjacatApplication.class, args);
   }
-  @Bean
-  public CommandLineRunner test(WarehouseService warehouseService, SaleService saleService, ApplicationUserService applicationUserService, PasswordEncoder passwordEncoder) {
-    return args -> {
-//      ApplicationUser applicationUser = new ApplicationUser("admin", "21RoOm7_");
-//      applicationUserService.registerUser(applicationUser, ApplicationUserRole.ADMIN);
-//      warehouseService.addWarehouse(new Warehouse("kek", 21, 21));
-//      saleService.addSale(new Sale(21, 21, new Date(), warehouseService.loadWarehouseById(3L)));
-//      saleService.updateSaleById(2L, new Sale(5, 1, new Date(), warehouseService.loadWarehouseById(1L)));
-//      saleService.deleteSaleById(2L);
-//      warehouseService.loadWarehouseById(1L).getSales().forEach(sale -> System.out.println(sale.getAmount()));
-//      System.out.println(warehouseService.loadAllWarehouses());
-//      saleService.loadAllSales().forEach(sale -> System.out.println(sale.getId()));
-//      System.out.println(saleService.loadAllSales());
-
-//      warehouseService.loadWarehouseById(1L).getSales().forEach(System.out::println);
-//      System.out.println(saleService.loadAllSales().get(0).getWarehouse());
-    };
-  }
 }
