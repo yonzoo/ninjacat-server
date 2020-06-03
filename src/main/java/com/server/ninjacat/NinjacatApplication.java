e77183a9286963b0f5fa9970b49c43bcc62f0d16
iamnzrv <nzrvofficial@gmail.com>
2020-06-03 21:33:19 +0300

    db params added

--- src/main/java/com/server/ninjacat/NinjacatApplication.java
+++ src/main/java/com/server/ninjacat/NinjacatApplication.java
@@ -1,28 +1,12 @@
 package com.server.ninjacat;
 
-import com.server.ninjacat.data.model.users.ApplicationUser;
-import com.server.ninjacat.data.service.things.SaleService;
-import com.server.ninjacat.data.service.things.WarehouseService;
-import com.server.ninjacat.data.service.users.ApplicationUserService;
-import com.server.ninjacat.security.roles.ApplicationUserRole;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NinjacatApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(NinjacatApplication.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner test(WarehouseService warehouseService, SaleService saleService, ApplicationUserService applicationUserService, PasswordEncoder passwordEncoder) {
-    return args -> {
-      ApplicationUser applicationUser = new ApplicationUser("admin", "21RoOm7_");
-      applicationUserService.registerUser(applicationUser, ApplicationUserRole.ADMIN);
-    };
   }
 }
